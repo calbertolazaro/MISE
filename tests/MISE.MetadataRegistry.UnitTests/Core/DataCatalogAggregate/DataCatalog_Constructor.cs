c851efe573b07b33f1d222cdadc184ac64c26ea7
calbertolazaro <calbertolazaro@tecnico.ulisboa.pt>
2021-09-27 18:38:35 +0100

    Setup units test project

---
+++ tests/MISE.MetadataRegistry.UnitTests/Core/DataCatalogAggregate/DataCatalog_Constructor.cs
@@ -0,0 +1,29 @@
+﻿using MISE.MetadataRegistry.Core.DataCatalogAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MISE.MetadataRegistry.UnitTests.Core.DataCatalogAggregate
+{
+    public class DataCatalog_Constructor
+    {
+        private string _title = "title of data catalog";
+        private string _description = "some useful description about the data catalog";
+        private DataCatalog _dataCatalog = null;
+
+        private DataCatalog CreateCatalog()
+        {
+            return new DataCatalog(_title, _description);
+        }
+
+        [Fact]
+        public void InitializeTitleAndDescription()
+        {
+            _dataCatalog = CreateCatalog();
+
+            Assert.Equal(_title, _dataCatalog.Title);
+            Assert.Equal(_description, _dataCatalog.Description);
+        }
+    }
+}
